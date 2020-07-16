743315622470dfb0b5aa9f5e80be9a5c8e6f7274
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-16 19:22:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,6 @@
 def prime?(number)
   return false if number < 2 
   return true if number == 2 
-  (2..number-1).each {|int| return false if number % int == 0}
-  true 
+  (2..number-1).none? {|int| number % int == 0}
 end   
   
